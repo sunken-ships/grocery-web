a6f3ed4e9bded3f69c29f74ca78bf43f19db11ba
Murtaza Saifuddin <mortuze@gmail.com>
2025-08-10 12:14:11 -0400

    account + demo base page

---
+++ src/app/account/page.tsx
@@ -0,0 +1,12 @@
+import Nav from "@/components/Nav";
+import React from "react";
+
+export default function AccountPage() {
+  return (
+    <div className="flex flex-col h-screen">
+      <Nav />
+
+      <h1>Account</h1>
+    </div>
+  );
+}
---
+++ src/app/demo/page.tsx
@@ -0,0 +1,11 @@
+import Nav from "@/components/Nav";
+import React from "react";
+
+export default function DemoPage() {
+  return (
+    <div className="flex flex-col min-h-screen ">
+      <Nav />
+      <p>Create a recipe and see it priced out.</p>
+    </div>
+  );
+}
